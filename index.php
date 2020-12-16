f8b61bbdc50bdf6af90bb1c6fdf8375f8af63c7a
Andrea <andre98377@gmail.com>
2020-12-16 15:26:16 +0100

    php-google-faq

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-google-faq</title>
+    <!-- Main style -->
+    <link rel="stylesheet" href="./dist/css/style.css">
+</head>
+<body>
+    
+    <!-- App -->
+    <div id="app">
+        <header>
+            <!-- logo -->
+            <img src="./img/google-logo.png" alt="logo">
+            <h1>Privacy e termini</h1>
+            <!-- Main menu -->
+        </header>
+
+        <!-- Main -->
+        <main>
+
+        </main>
+    </div>
+
+
+</body>
+</html>
